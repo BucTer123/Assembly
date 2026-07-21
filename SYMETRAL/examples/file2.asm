44331592936cabe10918bfc5919e06507e8cc044
SHALAWA <bambukvova3@gmail.com>
2026-07-21 16:03:09 +0300

    Create file2.asm

---
+++ SYMETRAL/examples/file2.asm
@@ -0,0 +1,14 @@
+%include "out.asm" ;Get code from out.asm
+
+section .text
+global _main ;Create manin function
+
+_main:
+  call _print_helloworld ; start SYMETRAL
+
+  call _input ; input text (but text you can edit in out.asm file)
+  call _create_variable ; create 'variable'
+  call _print ; create label (but text you can edit in out.asm file)
+  
+  call _print_endworld ;end SYMETRAL
+  call _exit ; SHUTDOWN
